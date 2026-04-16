a8a64e41dfe6f2c4f5cb201deb024d6f70197ed7
closurebit <ilya.golovach1508@gmail.com>
2026-04-16 20:45:50 +0300

    feat(project): add CI and plugins

--- src/main/java/io/github/closurebit/AssistantRun.java
+++ src/main/java/io/github/closurebit/AssistantRun.java
@@ -1,6 +1,6 @@
 package io.github.closurebit;
 
-public class AssitantRun {
+public class AssistantRun {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
